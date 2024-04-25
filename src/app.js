195fb8c7820ab66cf2a0a4fdae456afb8625a04c
Renali Keliune <108136063+AsuariFoxstein@users.noreply.github.com>
2024-04-25 16:24:20 +0000

    "My Apoligies, I hope this is good!"

--- src/app.js
+++ src/app.js
@@ -9,3 +9,34 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+const pronoun = 
+    ['the',
+     'our',
+     'dellis',
+     'chelsas',
+     'mays',
+     'truckers',
+     'tattoracks'
+    ];
+
+const adj = 
+    ['great', 'big', 'red','hole','round','super','green','dirty'];
+
+const noun = 
+    ['jogger', 'racoon', 'Avali', 'cheeseburger', 'violin','inn','sigil','homes','blacksmith'];
+
+const ext = 
+    ['.com', '.gov', '.co.uk', '.org', '.hrg', '.aaa'];
+   
+let sentence =
+pronoun[rand(pronoun.length)] +
+adj[rand(adj.length)] +
+noun[rand(noun.length)] + 
+ext[rand(ext.length)];
+
+console.log(sentence);
+
+function rand(length) {
+    let number = Math.floor(Math.random() * length);
+    return number;
+  };
